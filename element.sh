862c9d328c60c77825b3602f3e40b1e05cc51dae
Rexxis <David.harendza@gmail.com>
2022-11-10 09:07:50 +0000

    feat: added search from symbol and name functionality

--- element.sh
+++ element.sh
@@ -17,7 +17,18 @@ IS_NUMBER() {
 }
 
 IS_STRING() {
-  echo $1 is a string
+    # get data
+  DATA=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
+  if [[ -z $DATA ]]
+  then
+    echo "I could not find that element in the database"
+  else
+    # display the data
+    echo "$DATA" | while read TID BAR ANUMBER BAR SYMBOL BAR NAME BAR AMASS BAR MPOINT BAR BPOINT BAR TYPE
+    do
+      echo "The element with atomic number $ANUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $AMASS amu. $NAME has a melting point of $MPOINT celsius and a boiling point of $BPOINT celsius."
+    done
+  fi
 }
 
 if [[ "$1" =~ ^[0-9]+$ ]]; then
