e26a7486d75e7ce483625d0d4e4593d3ed052a60
Rexxis <David.harendza@gmail.com>
2022-11-10 08:53:10 +0000

    feat: added PSQL var and argument differentiator

--- element.sh
+++ element.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+IS_NUMBER() {
+  echo $1 is a number
+}
+
+IS_STRING() {
+  echo $1 is a string
+}
+
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+  IS_NUMBER "$1" >&2
+else
+  IS_STRING "$1" >&2
+fi
